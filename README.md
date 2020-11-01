0a9effc4efec000bf1ead95de0ad4663eb77de38
Starcush <hacker0419@gmail.com>
2020-11-01 18:30:14 +0900

    ADD eslint & prettier configuration

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # AA-frontend
+### clone후 프로젝트 시작하기
+- git clone이 완성되면 aa-frontend 폴더로 이동 후 npm 설치
+```
+cd aa-frontend
+npm install
+```
+- 설치가 완료되면 시작
+```
+npm start
+```
+
+### branch 생성 주의사항
+- nickname/feature/"기능 혹은 페이지 이름으로 브랜치 이름 지정"
+
+### git commit시 주의사항
+#### commit전
+- eslint와 prettier 적용 후 commit 할 것
+```
+npm run format // prettier 적용
+npm run lint // eslint 적용
+```
+
+#### commit message
+- 새로운 기능을 추가 했을 시 commit -m 'ADD 추가한 기능에 대한 간단한 설명'
+- 이미 있는 기능을 수정 했을 시 commit -m 'FIX 수정한 기능에 대한 간단한 설명'
+- 발생한 오류를 수정 했을 시 commit -m 'BUG 수정한 오류에 대한 간단한 설명'
---
+++ aa-frontend/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:import/errors', 'plugin:import/warnings', 'prettier', 'prettier/react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier', 'html'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': ['error', { skipUndeclared: true }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn',
+    // allow async-await
+    'generator-star-spacing': 0
+  },
+  parser: 'babel-eslint',
+  settings: {
+    react: {
+      createClass: 'createReactClass', // Regex for Component Factory to use,
+      // default to "createReactClass"
+      pragma: 'React', // Pragma to use, default to "React"
+      fragment: 'Fragment', // Fragment to use (may be a property of <pragma>), default to "Fragment"
+      version: 'detect', // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      // default to latest and warns if missing
+      // It will default to "detect" in the future
+      flowVersion: '0.53' // Flow version
+    },
+    propWrapperFunctions: [
+      // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+      'forbidExtraProps',
+      { property: 'freeze', object: 'Object' },
+      { property: 'myFavoriteWrapper' }
+    ],
+    linkComponents: [
+      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+      'Hyperlink',
+      { name: 'Link', linkAttribute: 'to' }
+    ]
+  }
+};
