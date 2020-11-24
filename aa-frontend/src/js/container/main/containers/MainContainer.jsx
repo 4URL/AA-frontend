35d8b136eff21ad530efbf7f93aed7106e6d214e
Starcush <hacker0419@gmail.com>
2020-11-25 00:03:58 +0900

    ADD react router and make 404, main, map files

---
+++ aa-frontend/src/js/container/main/containers/MainContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Map from './MapContainer';
+
+const Main = () => {
+  return <Map />;
+};
+
+export default Main;
---
+++ aa-frontend/src/js/container/main/containers/MapContainer.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Map = () => {
+  return (
+    <Fragment>
+      <h2>Map</h2>
+    </Fragment>
+  );
+};
+
+export default Map;
