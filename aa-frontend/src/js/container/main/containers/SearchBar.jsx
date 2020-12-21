2aba6ff141c73ab394e0cfa6b3a802799fade19d
hyo-il <yhug76@gmail.com>
2020-12-21 22:03:30 +0900

    add Search Component

--- aa-frontend/src/js/container/main/containers/SearchBar.jsx
+++ aa-frontend/src/js/container/main/containers/SearchBar.jsx
@@ -3,7 +3,10 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { SearchBarContainer, SearchBarDiv, SearchBarInput, SearchBarIcon, SearchIcon } from '../views/StyledComponents';
+// import { SearchBarContainer, SearchBarDiv, SearchBarInput, SearchBarIcon } from '../views/StyledComponents';
 import { changeLocation } from '../../../redux/actions/index';
+
+import SearchContainer from '../../search/container/SearchContainer';
 
 const SearchBar = props => {
   const [location, setLocation] = useState('');
@@ -18,16 +21,22 @@ const SearchBar = props => {
   };
 
   return (
-    <SearchBarContainer>
-      <SearchBarDiv size={6}>
-        <SearchBarInput type="text" placeholder="지도 검색" onChange={e => updateInput(e.target.value)} />
-      </SearchBarDiv>
+    // <SearchBarContainer>
+    //   <SearchBarDiv size={6}>
+    //     <SearchBarInput type="text" placeholder="지도 검색" onChange={e => updateInput(e.target.value)} />
+    //   </SearchBarDiv>
 
-      <SearchBarIcon size={1}>
-        <SearchIcon onClick={() => handleSearchLocation(props)} />
-      </SearchBarIcon>
-    </SearchBarContainer>
+    //   <SearchBarIcon size={1}>
+    //     <SearchIcon onClick={() => handleSearchLocation(props)} />
+    //   </SearchBarIcon>
+
+    // </SearchBarContainer>
+    <SearchContainer />
   );
+};
+
+const mapStateToProps = state => {
+  return state;
 };
 
 const mapDispatchToProps = dispatch => {
@@ -40,6 +49,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(
-  null, // mapStateToProps: store와 props가 연결
+  mapStateToProps, // mapStateToProps: store와 props가 연결
   mapDispatchToProps // mapDispatchProps: action과 props가 연결
 )(SearchBar);
